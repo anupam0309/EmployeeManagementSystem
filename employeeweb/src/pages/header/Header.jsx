d72633397a56e48f94c2c5f160db6db6ceed9bc8
Anupam Tiwary <anurise999@gmail.com>
2024-07-24 16:30:17 +0530

    Commit 1

---
+++ employeeweb/src/pages/header/Header.jsx
@@ -0,0 +1,23 @@
+import Container from "react-bootstrap/Container"
+import Navbar from "react-bootstrap/Navbar"
+import Nav from "react-bootstrap/Nav"
+import { Link } from "react-router-dom"
+import "./Header.css"
+
+const Header = () => {
+  return (
+    <>
+      <Navbar bg="primary" variant="dark">
+        <Container fluid>
+          <Navbar.Brand as={Link} to="/"><strong>Employee Management System</strong></Navbar.Brand>
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/" className="nav-link">Employee</Nav.Link>
+            <Nav.Link as={Link} to="/employee" className="nav-link">Post Employee</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  )
+}
+
+export default Header
